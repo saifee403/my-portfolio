c15239ca7682492a47ea09b5004d2019f0302bd6
saifee403 <muh.saif2001@gmail.com>
2023-09-07 23:00:37 -0700

    styling in services section

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,5 +1,25 @@
+import {content} from '../Content'
+
 const Services = () => {
-  return <div>Services</div>;
+  const {services} = content;
+  return <section>
+    <div className='md:container px-5 py-14'>
+      <h2 className='title'>{services.title}</h2>
+      <h4 className='subtitle'>{services.subtitle}</h4>
+      <br />
+      <div className=''>
+        {
+          services.service_content.map((content, i)=>(
+            <div key={i} className='min-w-[14rem] duration-300 cursor-pointer border-2 rounded-xl text-center bg-bg_light_primary p-6'>
+              <img src={content.logo} alt="..." />
+              <h6>{content.title}</h6>
+              <h6>{content.para}</h6>
+            </div>
+          ))
+        }
+      </div>
+    </div>
+  </section>;
 };
 
 export default Services;
